b7f6cc149becde20e737251192afb69fce36b9c9
wojakpepechad <tokenalert7@gmail.com>
2024-02-09 23:54:31 +0100

    Update useGetPepePrice.ts

--- src/utils/useGetPepePrice.ts
+++ src/utils/useGetPepePrice.ts
@@ -1,23 +1,5 @@
-import { useState, useEffect } from 'react';
-
 const useGetPepePrice = () => {
-  const [price, setPrice] = useState<number | 0>();
-
-  useEffect(() => {
-    const fetchPrice = async () => {
-      try {
-        const response = await fetch('https://app.geckoterminal.com/api/p1/eth/pools/0xa84181f223a042949e9040e42b44c50021802db6');
-        const data = await response.json();
-        setPrice(data.data.attributes.price_in_usd);
-      } catch (error) {
-        console.error('Error fetching PEPE price:', error);
-        setPrice(0); // Set to undefined in case of an error
-      }
-    };
-
-    fetchPrice();
-  }, []);
-
+  const price = 10.42;
   return price;
 };
 
